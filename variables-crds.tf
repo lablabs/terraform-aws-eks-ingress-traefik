1633b9dfb3fe1f1ad5c188e1d4e9c3f00abf2ff6
mliner <matej.liner@lablabs.io>
2026-02-16 13:29:19 +0100

    feat(sync): universal addon v1.0.0-rc1

--- variables-crds.tf
+++ variables-crds.tf
@@ -377,7 +377,10 @@ variable "crds_helm_set_sensitive" {
 }
 
 variable "crds_helm_postrender" {
-  type        = map(any)
+  type = object({
+    binary_path = string
+    args        = optional(list(string))
+  })
   default     = null
   description = "Value block with a path to a binary file to run after Helm renders the manifest which can alter the manifest contents. Defaults to `{}`."
 }
